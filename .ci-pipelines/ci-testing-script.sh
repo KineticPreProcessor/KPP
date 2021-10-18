39179a6aa84073326459e6249157504c8d4b3262
Bob Yantosca <yantosca@seas.harvard.edu>
2021-10-18 14:23:28 -0400

    Add extra CI tests: sdirk (sd) and runge_kutta (rk)
    Added two new CI test folders:
    ci-tests/sd (Sdirk integrator w/ F90)
    ci-tests/rk (Runge-Kutta integrator w/ F90)
    
    Also updated the .ci-pipelines/ci-testing-script.sh file
    accordingly to build & run these new tests.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- .ci-pipelines/ci-testing-script.sh
+++ .ci-pipelines/ci-testing-script.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+### CI tests for github.com/KineticPreProcessor/KPP ###
+
+# Build and run the runge-kutta (rk) test
+cd /kpp/ci-tests/rk
+../../bin/kpp rk.kpp
+make -f Makefile_rk COMPILER=GFORTRAN
+./rk.exe
+
+# Build and run the sdirk test
+cd /kpp/ci-tests/sd
+../../bin/kpp sd.kpp
+make -f Makefile_sd COMPILER=GFORTRAN
+./sd.exe
+
 # Build and run the small_f90 test
-cd /kpp/ci-tests/small.f90
+cd /kpp/ci-tests/small_f90
 ../../bin/kpp small_f90.kpp
 make -f Makefile_small_f90 COMPILER=GFORTRAN
 ./small_f90.exe
