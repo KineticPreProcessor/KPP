9d46c0fefc3574193fa7cecc07a6c54acfff8486
Bob Yantosca <yantosca@seas.harvard.edu>
2021-10-18 13:58:34 -0400

    Minor updates for the small_f90 CI test
    ./ci-pipelines/ci-testing-script.sh
    - Update commands for compiling & running
      the small_f90 CI test
    
    util/Makefile.f90
    - Bracket the compiler setting with an ifndef $(COMPILER)
      block.  If the COMPILER variable is not passed via the
      command line when calling "make", then it will default to
      COMPILER=INTEL.
    
    .gitignore_global
    - Removed from prior commit
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- .ci-pipelines/ci-testing-script.sh
+++ .ci-pipelines/ci-testing-script.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-cd /kpp/examples
-../bin/kpp small_f90.kpp
-make -f Makefile_small_f90
+
+# Build and run the small_f90 test
+cd /kpp/ci-tests/small.f90
+../../bin/kpp small_f90.kpp
+make -f Makefile_small_f90 COMPILER=GFORTRAN
+./small_f90.exe
--- util/Makefile.f90
+++ util/Makefile.f90
@@ -3,12 +3,15 @@
 #       Pedefined compilers: INTEL, PGF, HPUX, LAHEY
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# Pick a default compiler if not supplied
+ifndef COMPILER
 #COMPILER = G95
 #COMPILER = LAHEY
-#COMPILER = INTEL
+COMPILER = INTEL
 #COMPILER = PGF
 #COMPILER = HPUX
-COMPILER = GFORTRAN
+#COMPILER = GFORTRAN
+endif
 
 FC_G95     = g95
 FOPT_G95   = -cpp -O -pg -fbounds-check -fimplicit-none  -Wall -ftrace=full
@@ -19,7 +22,7 @@ FC_LAHEY   = lf95
 # More checking for debugging:
 FOPT_LAHEY = -Cpp --chk a,e,s,u --pca --ap -O0 -g --trap --trace --chkglobal
 
-FC_INTEL   = ifort 
+FC_INTEL   = ifort
 # More aggressive for production runs:
 #FOPT_INTEL = -cpp -O -fp-model precise -pc80 -prec_div
 # More checking for debugging:
@@ -46,20 +49,20 @@ FOPT = $(FOPT_$(COMPILER)) # -DFULL_ALGEBRA
 LIBS =
 #LIBS = -llapack -lblas
 
-# Command to create Matlab mex gateway routines 
+# Command to create Matlab mex gateway routines
 # Note: use $(FC) as the mex Fortran compiler
 MEX  = mex
 
 GENSRC = KPP_ROOT_Precision.f90  \
 	 KPP_ROOT_Parameters.f90     \
-	 KPP_ROOT_Global.f90  
+	 KPP_ROOT_Global.f90
 
 GENOBJ = KPP_ROOT_Precision.o    \
 	 KPP_ROOT_Parameters.o       \
-	 KPP_ROOT_Global.o     
+	 KPP_ROOT_Global.o
 
-FUNSRC = KPP_ROOT_Function.f90 
-FUNOBJ = KPP_ROOT_Function.o 
+FUNSRC = KPP_ROOT_Function.f90
+FUNOBJ = KPP_ROOT_Function.o
 
 JACSRC = KPP_ROOT_JacobianSP.f90  KPP_ROOT_Jacobian.f90
 JACOBJ = KPP_ROOT_JacobianSP.o    KPP_ROOT_Jacobian.o
@@ -67,20 +70,20 @@ JACOBJ = KPP_ROOT_JacobianSP.o    KPP_ROOT_Jacobian.o
 HESSRC = KPP_ROOT_HessianSP.f90   KPP_ROOT_Hessian.f90
 HESOBJ = KPP_ROOT_HessianSP.o     KPP_ROOT_Hessian.o
 
-STMSRC = KPP_ROOT_StoichiomSP.f90 KPP_ROOT_Stoichiom.f90 
+STMSRC = KPP_ROOT_StoichiomSP.f90 KPP_ROOT_Stoichiom.f90
 STMOBJ = KPP_ROOT_StoichiomSP.o   KPP_ROOT_Stoichiom.o
 
 UTLSRC = KPP_ROOT_Rates.f90 KPP_ROOT_Util.f90 KPP_ROOT_Monitor.f90
 UTLOBJ = KPP_ROOT_Rates.o   KPP_ROOT_Util.o   KPP_ROOT_Monitor.o
 
-LASRC  = KPP_ROOT_LinearAlgebra.f90 
-LAOBJ  = KPP_ROOT_LinearAlgebra.o   
+LASRC  = KPP_ROOT_LinearAlgebra.f90
+LAOBJ  = KPP_ROOT_LinearAlgebra.o
 
-STOCHSRC = KPP_ROOT_Stochastic.f90 
-STOCHOBJ = KPP_ROOT_Stochastic.o 
+STOCHSRC = KPP_ROOT_Stochastic.f90
+STOCHOBJ = KPP_ROOT_Stochastic.o
 
 MAINSRC = KPP_ROOT_Main.f90   KPP_ROOT_Initialize.f90   KPP_ROOT_Integrator.f90 KPP_ROOT_Model.f90
-MAINOBJ = KPP_ROOT_Main.o     KPP_ROOT_Initialize.o     KPP_ROOT_Integrator.o   KPP_ROOT_Model.o 
+MAINOBJ = KPP_ROOT_Main.o     KPP_ROOT_Initialize.o     KPP_ROOT_Integrator.o   KPP_ROOT_Model.o
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # User: modify the line below to include only the
@@ -95,7 +98,7 @@ ALLOBJ = $(GENOBJ) $(FUNOBJ) $(JACOBJ) $(HESOBJ) $(STMOBJ) \
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 all:    exe
 
-exe:	$(ALLOBJ) $(MAINOBJ) 
+exe:	$(ALLOBJ) $(MAINOBJ)
 	$(FC) $(FOPT) $(ALLOBJ) $(MAINOBJ) $(LIBS) -o KPP_ROOT.exe
 
 stochastic:$(ALLOBJ) $(STOCHOBJ) $(MAINOBJ)
@@ -117,7 +120,7 @@ distclean:
 	KPP_ROOT*.dat KPP_ROOT.exe KPP_ROOT.map \
 	KPP_ROOT*.f90 KPP_ROOT_*.mexglx
 
-KPP_ROOT_Precision.o: KPP_ROOT_Precision.f90 
+KPP_ROOT_Precision.o: KPP_ROOT_Precision.f90
 	$(FC) $(FOPT) -c $<
 
 KPP_ROOT_Parameters.o: KPP_ROOT_Parameters.f90 \
@@ -132,13 +135,13 @@ KPP_ROOT_Global.o: KPP_ROOT_Global.f90 \
 	            KPP_ROOT_Parameters.o KPP_ROOT_Precision.o
 	$(FC) $(FOPT) -c $<
 
-KPP_ROOT_Initialize.o: KPP_ROOT_Initialize.f90  $(GENOBJ) 
+KPP_ROOT_Initialize.o: KPP_ROOT_Initialize.f90  $(GENOBJ)
 	$(FC) $(FOPT) -c $<
 
-KPP_ROOT_Function.o: KPP_ROOT_Function.f90  $(GENOBJ) 
+KPP_ROOT_Function.o: KPP_ROOT_Function.f90  $(GENOBJ)
 	$(FC) $(FOPT) -c $<
 
-KPP_ROOT_Stochastic.o: KPP_ROOT_Stochastic.f90  $(GENOBJ) 
+KPP_ROOT_Stochastic.o: KPP_ROOT_Stochastic.f90  $(GENOBJ)
 	$(FC) $(FOPT) -c $<
 
 KPP_ROOT_JacobianSP.o: KPP_ROOT_JacobianSP.f90 $(GENOBJ)
@@ -150,7 +153,7 @@ KPP_ROOT_Jacobian.o: KPP_ROOT_Jacobian.f90  $(GENOBJ) KPP_ROOT_JacobianSP.o
 KPP_ROOT_LinearAlgebra.o: KPP_ROOT_LinearAlgebra.f90 $(GENOBJ) KPP_ROOT_JacobianSP.o
 	$(FC) $(FOPT) -c $<
 
-KPP_ROOT_Rates.o: KPP_ROOT_Rates.f90  $(GENOBJ) 
+KPP_ROOT_Rates.o: KPP_ROOT_Rates.f90  $(GENOBJ)
 	$(FC) $(FOPT) -c $<
 
 KPP_ROOT_HessianSP.o: KPP_ROOT_HessianSP.f90  $(GENOBJ)
