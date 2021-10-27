841b26a4a48e52215b523b462b22eed8a02e172d
Bob Yantosca <yantosca@seas.harvard.edu>
2021-10-27 17:37:47 -0400

    Update ci-testing-script.sh to exit if processes return failure
    We now test the $? (the bash status variable) after each individual
    step of the CI-tests.  If the status returns nonzero, we exit the script
    with a failure status of 1.
    
    Also print out some descriptive text to stdout to let us know where
    we are in the testing sequence.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- .ci-pipelines/ci-testing-script.sh
+++ .ci-pipelines/ci-testing-script.sh
@@ -4,18 +4,42 @@
 
 # Build and run the runge-kutta (rk) test
 cd /kpp/ci-tests/rk
+echo "Generating rk test mechanism with KPP..."
 ../../bin/kpp rk.kpp
+[ $? -ne 0 ] && exit 1
+echo "Building the rk test..."
 make -f Makefile_rk COMPILER=GFORTRAN
+[ $? -ne 0 ] && exit 1
+echo "Running the rk test..."
 ./rk.exe
+[ $? -ne 0 ] && exit 1
+echo "rk test successful!"
+echo ""
 
 # Build and run the sdirk test
 cd /kpp/ci-tests/sd
+echo "Generating sd test mechanism with KPP..."
 ../../bin/kpp sd.kpp
+[ $? -ne 0 ] && exit 1
+echo "Building the sd test ..."
 make -f Makefile_sd COMPILER=GFORTRAN
+[ $? -ne 0 ] && exit 1
+echo "Running the sd test..."
 ./sd.exe
+[ $? -ne 0 ] && exit 1
+echo "sd test successful!"
+echo ""
 
 # Build and run the small_f90 test
 cd /kpp/ci-tests/small_f90
+echo "Generating the small_f90 test mechanism with KPP..."
 ../../bin/kpp small_f90.kpp
+[ $? -ne 0 ] && exit 1
+echo "Building the small_f90 test..."
 make -f Makefile_small_f90 COMPILER=GFORTRAN
+[ $? -ne 0 ] && exit 1
+echo "Running the small_f90 test..."
 ./small_f90.exe
+[ $? -ne 0 ] && exit 1
+echo "small_f90 test successful!"
+echo ""
