07406bc58f5ebcb3ae70e716cf907afa5b8a8010
Rolf Sander <rolf.sander@mpic.de>
2022-03-18 15:17:41 +0100

    int/oldies deleted

--- .ci-pipelines/ci-testing-script.sh
+++ .ci-pipelines/ci-testing-script.sh
@@ -1,21 +1,42 @@
 #!/bin/sh
 
-### CI tests for github.com/KineticPreProcessor/KPP ###
+########################################################################
+### CI tests for github.com/KineticPreProcessor/KPP                  ###
+########################################################################
 
-# Build and run the runge-kutta (rk) test
-cd /kpp/ci-tests/rk
-../../bin/kpp rk.kpp
-make -f Makefile_rk COMPILER=GFORTRAN
-./rk.exe
+# List of tests (add more as necessary; separate each with a space)
+all_tests="radau90 rk rktlm ros rosadj rosenbrock90 rostlm saprc2006 sd sdadj small_f90"
 
-# Build and run the sdirk test
-cd /kpp/ci-tests/sd
-../../bin/kpp sd.kpp
-make -f Makefile_sd COMPILER=GFORTRAN
-./sd.exe
+# Run each test
+# Check status of each individual operation and exit if any do not complete
+for this_test in $all_tests; do
 
-# Build and run the small_f90 test
-cd /kpp/ci-tests/small_f90
-../../bin/kpp small_f90.kpp
-make -f Makefile_small_f90 COMPILER=GFORTRAN
-./small_f90.exe
+    cd /kpp/ci-tests/$this_test
+    [ $? -ne 0 ] && exit 1
+
+    echo ""
+    echo ">>>>>>>> Generating $this_test mechanism with KPP <<<<<<<<"
+    echo ""
+    ../../bin/kpp $this_test.kpp
+    [ $? -ne 0 ] && exit 1
+
+    echo ""
+    echo ">>>>>>>> Building the $this_test test executable <<<<<<<<<"
+    echo ""
+    make -f Makefile_$this_test COMPILER=GFORTRAN
+    [ $? -ne 0 ] && exit 1
+
+    echo ""
+    echo ">>>>>>>> Running the $this_test test <<<<<<<<"
+    echo ""
+    ./$this_test.exe
+    [ $? -ne 0 ] && exit 1
+
+    echo ""
+    echo ">>>>>>>> $this_test test was successful! <<<<<<<<"
+    echo ""
+
+done
+
+# Return w/ success
+exit 0
