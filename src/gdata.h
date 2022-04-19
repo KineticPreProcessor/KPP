df80a5acbdecc3a7de37d8fce1dd92f081015958
Rolf Sander <rolf.sander@mpic.de>
2022-04-19 15:21:40 +0200

    prepare merge with dev

--- src/gdata.h
+++ src/gdata.h
@@ -29,7 +29,7 @@
 
 ******************************************************************************/
 
-#define KPP_VERSION "2.2.3_rs3"
+#define KPP_VERSION "2.2.3"
 
 #ifndef _GDATA_H_
 #define _GDATA_H_
