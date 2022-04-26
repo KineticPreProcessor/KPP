227ec79f02b156f10221a8b3e2436434cb455c5a
Bob Yantosca <yantosca@seas.harvard.edu>
2022-04-26 15:30:55 -0400

    Update driver files to avoid use of VAR
    drv/general_adj.f90
    drv/general_tlm.f90
    - Now pass C(1:NVAR) to the integrator routine (INTEGRATE_ADJ for adjoint,
      INTEGRATE_TLM for TLM) instead of VAR.  We have now updated KPP so that
      VAR and FIX are threadprivate pointers to C and that are assigned and
      nullified from within the integrator.
    - Also update comments for clarity
    - Trimmed trailing whitespaces
    
    drv/general.f90
    - Define ICNTRL local variable (passed to the integrator via ICNTRL_U)
    - Define RCNTRL local variable (passed to the integrator via RCNTRL_U)
    - Updated comments for clarity
    - Trimmed trailing whitespaces
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- drv/general.f90
+++ drv/general.f90
@@ -1,14 +1,23 @@
 PROGRAM KPP_ROOT_Driver
 
-  USE KPP_ROOT_Model
-  USE KPP_ROOT_Initialize, ONLY: Initialize
+!~~~> Global variables and routines
+      USE KPP_ROOT_Model
+      USE KPP_ROOT_Initialize, ONLY: Initialize
 
+!~~~> Local variables
       KPP_REAL :: T, DVAL(NSPEC)
       KPP_REAL :: RSTATE(20)
       INTEGER :: i
-  
-!~~~> Initialization 
 
+!~~~> Control (in) arguments for the integration
+!~~~> These are set to zero by default, which will invoke default behavior
+      INTEGER,  ICNTRL(20)
+      KPP_REAL, RCNTRL(20)
+
+      ICNTRL  = 0
+      RCNTRL  = 0.d0
+
+!~~~> Initialization
       STEPMIN = 0.0d0
       STEPMAX = 0.0d0
 
@@ -16,8 +25,11 @@ PROGRAM KPP_ROOT_Driver
         RTOL(i) = 1.0d-4
         ATOL(i) = 1.0d-3
       END DO
-     
+
+!~~~> Set default species conceentrations
       CALL Initialize()
+
+!~~~> Open log file for write
       CALL InitSaveData()
 
 !~~~> Time loop
@@ -25,25 +37,35 @@ PROGRAM KPP_ROOT_Driver
 kron: DO WHILE (T < TEND)
 
         TIME = T
+
+!~~~> Write values of monitored species at each iteration
         CALL GetMass( C, DVAL )
         WRITE(6,991) (T-TSTART)/(TEND-TSTART)*100, T,       &
                    ( TRIM(SPC_NAMES(MONITOR(i))),           &
                      C(MONITOR(i))/CFACTOR, i=1,NMONITOR )
         CALL SaveData()
-        CALL Update_SUN() 
+
+!~~~> Update sunlight intensity and reaction rates
+!~~~> before calling the integrator.
+        CALL Update_SUN()
         CALL Update_RCONST()
 
-        CALL INTEGRATE( TIN = T, TOUT = T+DT, RSTATUS_U = RSTATE, &
-        ICNTRL_U = (/ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 /) )
+!~~~> Call the integrator
+        CALL INTEGRATE( TIN       = T,        &
+                        TOUT      = T+DT,     &
+                        ICNTRL_U  = ICNTRL,   &
+                        RCNTRL_U  = RCNTRL,   &
+                        RSTATUS_U = RSTATE,  )
         T = RSTATE(1)
 
       END DO kron
 !~~~> End Time loop
 
+!~~~> Write final values of monitored species and close log file
       CALL GetMass( C, DVAL )
       WRITE(6,991) (T-TSTART)/(TEND-TSTART)*100, T,     &
                ( TRIM(SPC_NAMES(MONITOR(i))),           &
-                 C(MONITOR(i))/CFACTOR, i=1,NMONITOR ) 
+                 C(MONITOR(i))/CFACTOR, i=1,NMONITOR )
       TIME = T
       CALL SaveData()
       CALL CloseSaveData()
@@ -51,4 +73,3 @@ kron: DO WHILE (T < TEND)
 991   FORMAT(F6.1,'%. T=',E9.3,2X,200(A,'=',E11.4,'; '))
 
 END PROGRAM KPP_ROOT_Driver
-
