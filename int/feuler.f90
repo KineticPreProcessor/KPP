8fa2ff9b7994c303b2fbc1df0e635da4f5efb760
Bob Yantosca <yantosca@seas.harvard.edu>
2022-05-04 15:58:25 -0400

    Make the feuler integrator compatible with drv/driver.f90
    int/feuler.f90
    - Add code to test if Update_SUN or UPDATE_RCONST should be updated
      from within the integrator
    - Also pass RCNTRL_U and RSTATUS_U to function Integrate
    - In routine ForwardEuler, the error check for negatives is only done
      if ICNTRL(2) == 2.
    - Add pointers to VAR and FIX before the call to ForwardEuler (and
      free them immediately after the call)
    - Updated comments
    - Trimmed trailing whitespace
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- int/feuler.f90
+++ int/feuler.f90
@@ -8,108 +8,186 @@
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
 MODULE KPP_ROOT_Integrator
 
-  USE KPP_ROOT_Precision, ONLY: dp
+  USE KPP_ROOT_Precision,  ONLY: dp
   USE KPP_ROOT_Parameters, ONLY: NVAR, NFIX, NSPEC
   USE KPP_ROOT_Global
   USE KPP_ROOT_Monitor
+
   IMPLICIT NONE
   PUBLIC
+
   SAVE
 
-  ! description of the error numbers IERR
+!~~~> Flags to determine if we should call the UPDATE_* routines from within
+!~~~> the integrator.  If using KPP in an external model, you might want to
+!~~~> disable these calls (via ICNTRL(15)) to avoid excess computations.
+  LOGICAL, PRIVATE :: Do_Update_RCONST
+  LOGICAL, PRIVATE :: Do_Update_PHOTO
+  LOGICAL, PRIVATE :: Do_Update_SUN
+
+!~~~> description of the error numbers IERR
   CHARACTER(LEN=50), PARAMETER, DIMENSION(1) :: IERR_NAMES = (/ &
     'dummy value                                       ' /)
 
 CONTAINS
 
-  ! **************************************************************************
+  !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  SUBROUTINE INTEGRATE( TIN, TOUT, ICNTRL_U, IERR_U )
-    IMPLICIT NONE
-    
-    KPP_REAL, INTENT(IN) :: TIN  ! Start Time
-    KPP_REAL, INTENT(IN) :: TOUT ! End Time
-    ! Optional input parameters and statistics
-    INTEGER,       INTENT(IN),  OPTIONAL :: ICNTRL_U(20)
-    INTEGER,       INTENT(OUT), OPTIONAL :: IERR_U
+  SUBROUTINE Integrate( TIN, TOUT, ICNTRL_U, RCNTRL_U, RSTATUS_U, IERR_U )
+
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ! User interface routine to the KPP forward Euler integrator
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    USE KPP_ROOT_Util, ONLY : Integrator_Update_Options
+
+    !~~~> Inputs
+    KPP_REAL, INTENT(IN)            :: TIN           ! Start Time
+    KPP_REAL, INTENT(IN)            :: TOUT          ! End Time
+    INTEGER,  INTENT(IN),  OPTIONAL :: ICNTRL_U(20)  ! Input options
+    KPP_REAL, INTENT(IN),  OPTIONAL :: RCNTRL_U(20)  ! Input options
+
+    !~~~> Outputs
+    KPP_REAL, INTENT(OUT), OPTIONAL :: RSTATUS_U(20) ! Returned status values
+    INTEGER,  INTENT(OUT), OPTIONAL :: IERR_U        ! Error code
+
     ! Local variables
-    INTEGER                              :: ICNTRL(20)
-    INTEGER                              :: IERR
-
-    ! ICNTRL(1) = verbose error output
-    ! ICNTRL(2) = stop at negative integration result
+    INTEGER                         :: ICNTRL(20)
+    KPP_REAL                        :: RSTATUS(20)
+    INTEGER                         :: IERR
 
     !~~~> Zero input and output arrays for safety's sake
     ICNTRL     = 0
+    RSTATUS    = 0.0_dp
 
-    IF ( PRESENT( ICNTRL_U ) ) THEN
-       WHERE( ICNTRL_U /= 0 ) ICNTRL = ICNTRL_U
-    ENDIF
+    !~~~> fine-tune the integrator:
+    ICNTRL(1 ) = 0       ! Verbose error output
+    ICNTRL(2 ) = 0       ! Stop upon negative results
+    ICNTRL(15) = 5       ! Call Update_SUN and Update_RCONST from w/in the int.
 
-    CALL FORWARDEULER(NVAR,VAR,TIN,TOUT,ICNTRL,IERR)
+   !~~~> if optional parameters are given, and if they are /= 0,
+   !     then use them to overwrite default settings
+   IF ( PRESENT( ICNTRL_U ) ) THEN
+      WHERE( ICNTRL_U /= 0 ) ICNTRL = ICNTRL_U
+   ENDIF
 
-    IF ( PRESENT( IERR_U ) ) IERR_U  = IERR
+   !~~~> Determine the settings of the Do_Update_* flags, which determine
+   !~~~> whether or not we need to call Update_* routines in the integrator
+   !~~~> (or not, if we are calling them from a higher-level)
+   ! ICNTRL(15) = -1 ! Do not call Update_* functions within the integrator
+   !            =  0 ! Status quo
+   !            =  1 ! Call Update_RCONST from within the integrator
+   !            =  2 ! Call Update_PHOTO from within the integrator
+   !            =  3 ! Call Update_RCONST and Update_PHOTO from w/in the int.
+   !            =  4 ! Call Update_SUN from within the integrator
+   !            =  5 ! Call Update_SUN and Update_RCONST from within the int.
+   !            =  6 ! Call Update_SUN and Update_PHOTO from within the int.
+   !            =  7 ! Call Update_SUN, Update_PHOTO, Update_RCONST w/in int.
+   CALL Integrator_Update_Options( ICNTRL(15),          &
+                                   Do_Update_RCONST,    &
+                                   Do_Update_PHOTO,     &
+                                   Do_Update_Sun       )
 
-  END SUBROUTINE INTEGRATE
+   !~~~> In order to remove the prior EQUIVALENCE statements (which
+   !~~~> are not thread-safe), we now have declared VAR and FIX as
+   !~~~> threadprivate pointer variables that can point to C.
+   VAR => C(1:NVAR)
+   FIX => C(NVAR+1:NSPEC)
 
-  SUBROUTINE FORWARDEULER(N,Y,Tstart,Tend,ICNTRL,IERR)
+   !~~~> Call the integrator
+   CALL ForwardEuler( NVAR, C(1:NVAR), TIN, TOUT, ICNTRL, IERR )
+
+   !~~~> Free pointers
+   VAR => NULL()
+   FIX => NULL()
+
+   !~~~> Return error status
+   IF ( PRESENT( IERR_U    ) ) IERR_U    = IERR
+   IF ( PRESENT( RSTATUS_U ) ) RSTATUS_U = RSTATUS
+
+  END SUBROUTINE Integrate
+
+  !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  SUBROUTINE ForwardEuler( N, Y, Tstart, Tend, ICNTRL, IERR )
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    !  Forward Euler integrator
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    ! External functions
+    USE KPP_ROOT_Rates, ONLY : Update_SUN, Update_RCONST
 
     ! Input: Arguments
-    KPP_REAL, INTENT(INOUT) :: Y(N) ! initial condition
-    INTEGER,  INTENT(IN)    :: N
-    KPP_REAL, INTENT(IN)    :: Tstart,Tend
-    INTEGER,  INTENT(IN)    :: ICNTRL(20)
-    ! Output: 
-    INTEGER, INTENT(OUT)    :: IERR ! Error indicator
+    INTEGER,  INTENT(IN)    :: N            ! Dimension for Y(N)
+    KPP_REAL, INTENT(INOUT) :: Y(N)         ! Initial condition
+    KPP_REAL, INTENT(IN)    :: Tstart       ! Starting time
+    KPP_REAL, INTENT(IN)    :: Tend         ! Ending time
+    INTEGER,  INTENT(IN)    :: ICNTRL(20)   ! KPP integrator options
+
+    ! Output:
+    INTEGER, INTENT(OUT)    :: IERR         ! Error status
+
     ! Local variables
     INTEGER  :: i
     LOGICAL  :: hasNegative
     KPP_REAL :: Ynew(N), dYdt(N)
 
-    !
+    ! Initialize
     hasNegative = .false.
-    
+
+    ! Update rates before integration if desired
+    IF ( Do_Update_SUN    ) CALL Update_SUN()
+    IF ( Do_Update_RCONST ) CALL Update_RCONST()
+
     ! Get P-kY
-    CALL FunTEMPLATE( Y, dYdt )
+    CALL FunTemplate( Y, dYdt )
 
-    ! Integrate
-    Ynew = Y+dYdt*(Tend-Tstart)
+    !~~~> Do the integration
+    Ynew = Y + ( dYdt * ( Tend - Tstart ) )
 
-    ! Check for negatives
-    DO i=1,N
-       IF (Ynew(i) .lt. 0._dp) THEN
-          hasNegative = .true.
-          IF (ICNTRL(1) /= 0) write(*,*) trim(SPC_NAMES(i)), " is negative."
-          IERR = -9
-          IF (ICNTRL(2) /= 0) THEN
-             write(*,*) 'Stopping'
-             RETURN
+    !~~~> Check for negatives
+    !~~~> Skip check unless otherwise specified by ICNTRL(2)
+    IF ( ICNTRL(2) /= 0 ) THEN
+       DO i=1,N
+          IF (Ynew(i) .lt. 0._dp) THEN
+             hasNegative = .true.
+             IF (ICNTRL(1) /= 0) write(*,*) trim(SPC_NAMES(i)), " is negative."
+             IERR = -9
+             IF (ICNTRL(2) /= 0) THEN
+                IERR = -1
+                write(*,*) 'Stopping'
+                RETURN
+             ENDIF
           ENDIF
-       ENDIF
-    ENDDO
+       ENDDO
+    ENDIF
 
+    ! Return updated concentrations
     Y = Ynew
 
     ! Succesful exit
     IERR = 1
-    
-  END SUBROUTINE FORWARDEULER
 
-  !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  END SUBROUTINE ForwardEuler
+
+  !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
   SUBROUTINE FunTemplate( Y, Ydot )
-    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     !  Template for the ODE function call.
-    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    USE KPP_ROOT_Parameters, ONLY: NVAR
-    USE KPP_ROOT_Global,     ONLY: FIX, RCONST
-    USE KPP_ROOT_Function,   ONLY: Fun
-    
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    USE KPP_ROOT_Parameters, ONLY : NVAR
+    USE KPP_ROOT_Global,     ONLY : FIX, RCONST
+    USE KPP_ROOT_Function,   ONLY : Fun
+
     !~~~> Input variables
     KPP_REAL :: Y(NVAR)
+
     !~~~> Output variables
     KPP_REAL :: Ydot(NVAR)
 
-    CALL Fun( Y, FIX, RCONST, Ydot )
+    !~~~> Compute equation rates and time derivative of variable species
+    CALL Fun( Y, VAR, RCONST, Ydot )
 
   END SUBROUTINE FunTemplate
 
