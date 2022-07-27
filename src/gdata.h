5582485d6325b3eedae68b54aef7fc69b6594049
Bob Yantosca <yantosca@seas.harvard.edu>
2022-07-27 15:07:41 -0400

    Updated CHANGELOG.md ; Updated version number to 3.0.0-rc.3
    Added a sentence to the CHANGELOG.md to say that all authors from
    Lin et al 2022 have been added to the RTD front page as KPP authors.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- src/gdata.h
+++ src/gdata.h
@@ -29,7 +29,7 @@
 
 ******************************************************************************/
 
-#define KPP_VERSION "3.0.0-rc.2"
+#define KPP_VERSION "3.0.0-rc.3"
 
 #ifndef _GDATA_H_
 #define _GDATA_H_
