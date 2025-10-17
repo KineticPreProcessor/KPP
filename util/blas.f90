3b756f4a63762acb94a6247b39aa1be676edb7e6
Bob Yantosca <yantosca@seas.harvard.edu>
2025-10-17 15:18:09 -0400

    Removed bsolete BLAS functions from util/blas.f90
    util/blas.f90
    - Removed the following BLAS functions, which have been replaced
      by pure F90 code in the various F90 integrator modules:
      (1) WCOPY
      (2) WAXPY
      (3) WSCAL
      (4) WLAMCH
      (5) WLAMCH_ADD
      (6) SET2ZERO
      (7) WADD
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- util/blas.f90
+++ util/blas.f90
@@ -8,225 +8,21 @@
 !      Virginia Polytechnic Institute and State University
 !--------------------------------------------------------------
 
-
-!--------------------------------------------------------------
-      SUBROUTINE WCOPY(N,X,incX,Y,incY)
-!--------------------------------------------------------------
-!     copies a vector, x, to a vector, y:  y <- x
-!     only for incX=incY=1
-!     after BLAS
-!     replace this by the function from the optimized BLAS implementation:
-!         CALL  SCOPY(N,X,1,Y,1)   or   CALL  DCOPY(N,X,1,Y,1)
-!--------------------------------------------------------------
-!     USE KPP_ROOT_Precision
-      
-      INTEGER  :: i,incX,incY,M,MP1,N
-      KPP_REAL :: X(N),Y(N)
-
-      IF (N.LE.0) RETURN
-
-      M = MOD(N,8)
-      IF( M .NE. 0 ) THEN
-        DO i = 1,M
-          Y(i) = X(i)
-        END DO
-        IF( N .LT. 8 ) RETURN
-      END IF    
-      MP1 = M+1
-      DO i = MP1,N,8
-        Y(i) = X(i)
-        Y(i + 1) = X(i + 1)
-        Y(i + 2) = X(i + 2)
-        Y(i + 3) = X(i + 3)
-        Y(i + 4) = X(i + 4)
-        Y(i + 5) = X(i + 5)
-        Y(i + 6) = X(i + 6)
-        Y(i + 7) = X(i + 7)
-      END DO
-
-      END SUBROUTINE WCOPY
-
-
-!--------------------------------------------------------------
-      SUBROUTINE WAXPY(N,Alpha,X,incX,Y,incY)
-!--------------------------------------------------------------
-!     constant times a vector plus a vector: y <- y + Alpha*x
-!     only for incX=incY=1
-!     after BLAS
-!     replace this by the function from the optimized BLAS implementation:
-!         CALL SAXPY(N,Alpha,X,1,Y,1) or  CALL DAXPY(N,Alpha,X,1,Y,1)
-!--------------------------------------------------------------
-
-      INTEGER  :: i,incX,incY,M,MP1,N
-      KPP_REAL :: X(N),Y(N),Alpha
-      KPP_REAL, PARAMETER :: ZERO = 0.0_dp
-
-      IF (Alpha .EQ. ZERO) RETURN
-      IF (N .LE. 0) RETURN
-
-      M = MOD(N,4)
-      IF( M .NE. 0 ) THEN
-        DO i = 1,M
-          Y(i) = Y(i) + Alpha*X(i)
-        END DO
-        IF( N .LT. 4 ) RETURN
-      END IF
-      MP1 = M + 1
-      DO i = MP1,N,4
-        Y(i) = Y(i) + Alpha*X(i)
-        Y(i + 1) = Y(i + 1) + Alpha*X(i + 1)
-        Y(i + 2) = Y(i + 2) + Alpha*X(i + 2)
-        Y(i + 3) = Y(i + 3) + Alpha*X(i + 3)
-      END DO
-      
-      END SUBROUTINE WAXPY
-
-
-
-!--------------------------------------------------------------
-      SUBROUTINE WSCAL(N,Alpha,X,incX)
-!--------------------------------------------------------------
-!     constant times a vector: x(1:N) <- Alpha*x(1:N) 
-!     only for incX=incY=1
-!     after BLAS
-!     replace this by the function from the optimized BLAS implementation:
-!         CALL SSCAL(N,Alpha,X,1) or  CALL DSCAL(N,Alpha,X,1)
-!--------------------------------------------------------------
-
-      INTEGER  :: i,incX,M,MP1,N
-      KPP_REAL  :: X(N),Alpha
-      KPP_REAL, PARAMETER  :: ZERO=0.0_dp, ONE=1.0_dp
-
-      IF (Alpha .EQ. ONE) RETURN
-      IF (N .LE. 0) RETURN
-
-      M = MOD(N,5)
-      IF( M .NE. 0 ) THEN
-        IF (Alpha .EQ. (-ONE)) THEN
-          DO i = 1,M
-            X(i) = -X(i)
-          END DO
-        ELSEIF (Alpha .EQ. ZERO) THEN
-          DO i = 1,M
-            X(i) = ZERO
-          END DO
-        ELSE
-          DO i = 1,M
-            X(i) = Alpha*X(i)
-          END DO
-        END IF
-        IF( N .LT. 5 ) RETURN
-      END IF
-      MP1 = M + 1
-      IF (Alpha .EQ. (-ONE)) THEN
-        DO i = MP1,N,5
-          X(i)     = -X(i)
-          X(i + 1) = -X(i + 1)
-          X(i + 2) = -X(i + 2)
-          X(i + 3) = -X(i + 3)
-          X(i + 4) = -X(i + 4)
-        END DO
-      ELSEIF (Alpha .EQ. ZERO) THEN
-        DO i = MP1,N,5
-          X(i)     = ZERO
-          X(i + 1) = ZERO
-          X(i + 2) = ZERO
-          X(i + 3) = ZERO
-          X(i + 4) = ZERO
-        END DO
-      ELSE
-        DO i = MP1,N,5
-          X(i)     = Alpha*X(i)
-          X(i + 1) = Alpha*X(i + 1)
-          X(i + 2) = Alpha*X(i + 2)
-          X(i + 3) = Alpha*X(i + 3)
-          X(i + 4) = Alpha*X(i + 4)
-        END DO
-      END IF
-
-      END SUBROUTINE WSCAL
-
-!--------------------------------------------------------------
-      KPP_REAL FUNCTION WLAMCH( C )
-!--------------------------------------------------------------
-!     returns epsilon machine
-!     after LAPACK
-!     replace this by the function from the optimized LAPACK implementation:
-!          CALL SLAMCH('E') or CALL DLAMCH('E')
-!--------------------------------------------------------------
-!      USE KPP_ROOT_Precision
-
-      CHARACTER ::  C
-      INTEGER    :: i
-      KPP_REAL, SAVE  ::  Eps
-      KPP_REAL  ::  Suma
-      KPP_REAL, PARAMETER  ::  ONE=1.0_dp, HALF=0.5_dp
-      LOGICAL, SAVE   ::  First=.TRUE.
-
-!$OMP THREADPRIVATE( Eps, First )      
-
-      IF (First) THEN
-        First = .FALSE.
-        Eps = HALF**(16)
-        DO i = 17, 80
-          Eps = Eps*HALF
-          CALL WLAMCH_ADD(ONE,Eps,Suma)
-          IF (Suma.LE.ONE) GOTO 10
-        END DO
-        PRINT*,'ERROR IN WLAMCH. EPS < ',Eps
-        RETURN
-10      Eps = Eps*2
-        i = i-1      
-      END IF
-
-      WLAMCH = Eps
-
-      END FUNCTION WLAMCH
-     
-      SUBROUTINE WLAMCH_ADD( A, B, Suma )
-!      USE KPP_ROOT_Precision
-      
-      KPP_REAL A, B, Suma
-      Suma = A + B
-
-      END SUBROUTINE WLAMCH_ADD
-!--------------------------------------------------------------
-
-
-!--------------------------------------------------------------
-      SUBROUTINE SET2ZERO(N,Y)
-!--------------------------------------------------------------
-!     copies zeros into the vector y:  y <- 0
-!     after BLAS
-!--------------------------------------------------------------
-      
-      INTEGER ::  i,M,MP1,N
-      KPP_REAL ::  Y(N)
-      KPP_REAL, PARAMETER :: ZERO = 0.0d0
-
-      IF (N.LE.0) RETURN
-
-      M = MOD(N,8)
-      IF( M .NE. 0 ) THEN
-        DO i = 1,M
-          Y(i) = ZERO
-        END DO
-        IF( N .LT. 8 ) RETURN
-      END IF    
-      MP1 = M+1
-      DO i = MP1,N,8
-        Y(i)     = ZERO
-        Y(i + 1) = ZERO
-        Y(i + 2) = ZERO
-        Y(i + 3) = ZERO
-        Y(i + 4) = ZERO
-        Y(i + 5) = ZERO
-        Y(i + 6) = ZERO
-        Y(i + 7) = ZERO
-      END DO
-
-      END SUBROUTINE SET2ZERO
-
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+!%%% NOTE: The following BLAS functions have been removed, %%%
+!%%% as they now have been replaced by pure F90 code       %%%
+!%%% in the various integrator modules;                    %%%
+!%%%                                                       %%%
+!%%% (1) WCOPY                                             %%%
+!%%% (2) WAXPY                                             %%%
+!%%% (3) WSCAL                                             %%%
+!%%% (4) WLAMCH                                            %%%
+!%%% (5) WLAMCH_ADD                                        %%%
+!%%% (6) SET2ZERO                                          %%%
+!%%% (7) WADD                                              %%%
+!%%%                                                       %%%
+!%%% @yantosca, 17 Oct 2025                                %%%
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 !--------------------------------------------------------------
       KPP_REAL FUNCTION WDOT (N, DX, incX, DY, incY) 
@@ -288,40 +84,6 @@
 
       END FUNCTION WDOT                                          
 
-
-!--------------------------------------------------------------
-      SUBROUTINE WADD(N,X,Y,Z)
-!--------------------------------------------------------------
-!     adds two vectors: z <- x + y
-!     BLAS - like
-!--------------------------------------------------------------
-!     USE KPP_ROOT_Precision
-      
-      INTEGER :: i, M, MP1, N
-      KPP_REAL :: X(N),Y(N),Z(N)
-
-      IF (N.LE.0) RETURN
-
-      M = MOD(N,5)
-      IF( M /= 0 ) THEN
-         DO i = 1,M
-            Z(i) = X(i) + Y(i)
-         END DO
-         IF( N < 5 ) RETURN
-      END IF    
-      MP1 = M+1
-      DO i = MP1,N,5
-         Z(i)     = X(i)     + Y(i)
-         Z(i + 1) = X(i + 1) + Y(i + 1)
-         Z(i + 2) = X(i + 2) + Y(i + 2)
-         Z(i + 3) = X(i + 3) + Y(i + 3)
-         Z(i + 4) = X(i + 4) + Y(i + 4)
-      END DO
-
-      END SUBROUTINE WADD
-      
-      
-      
 !--------------------------------------------------------------
       SUBROUTINE WGEFA(N,A,Ipvt,info)
 !--------------------------------------------------------------
